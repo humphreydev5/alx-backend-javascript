dfc109dc6c7cd3d131c61e8e830341c293314b24
humphreydev5 <humphreyikhalea^@gmail.com>
2024-08-21 23:33:02 +0100

    Complete

--- 0x05-Node_JS_basic/full_server/controllers/AppController.js
+++ 0x05-Node_JS_basic/full_server/controllers/AppController.js
@@ -1,6 +1,6 @@
 /**
  * Contains the miscellaneous route handlers.
- * @author Bezaleel Olakunori <https://github.com/B3zaleel>
+ * @author Humphrey Ikhalea <https://github.com/humphreydev5>
  */
 class AppController {
   static getHomepage(request, response) {
--- 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
+++ 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
@@ -7,7 +7,7 @@ const VALID_MAJORS = ['CS', 'SWE'];
 
 /**
  * Contains the student-related route handlers.
- * @author Bezaleel Olakunori <https://github.com/B3zaleel>
+ * @author Humphrey Ikhalea <https://github.com/humphreydev5>
  */
 class StudentsController {
   static getAllStudents(request, response) {
